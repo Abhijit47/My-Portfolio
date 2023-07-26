40d818fbd0ce96778cae8fc6ff8b855b3da2c4b3
Abhijit <akarmakar846@gmail.com>
2023-07-26 16:29:52 +0530

    Added more projects and some other changes

---
+++ script.js
@@ -0,0 +1,16 @@
+const app = document.getElementById('app');
+
+const typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString('A simple yet powerful native javascript')
+  .pauseFor(300)
+  .deleteChars(10)
+  .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
+  .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
+  .pauseFor(1000)
+  .start();
